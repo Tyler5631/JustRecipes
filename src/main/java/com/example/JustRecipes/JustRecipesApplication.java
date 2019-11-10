bbc6b513fd7473f09048951bb6afce3072163194
Tyler Cronin <tyler_cronin@yahoo.com>
2019-11-10 14:47:46 -0500

    init

---
+++ src/main/java/com/example/JustRecipes/JustRecipesApplication.java
@@ -0,0 +1,13 @@
+package com.example.JustRecipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustRecipesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustRecipesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JustRecipes/JustRecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JustRecipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustRecipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
